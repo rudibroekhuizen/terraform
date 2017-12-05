335655a3ce034b9c63b802b7075a0d3bd701007e
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-12-05 10:05:43 +0100

    Create instance.tf

---
+++ packet/waarneming/instance.tf
@@ -0,0 +1,19 @@
+# Configure the Packet Provider
+provider "packet" {
+  auth_token = "${var.auth_token}"
+}
+
+# Create a project
+resource "packet_project" "waarneming" {
+  name = "waarneming"
+}
+
+# Create a device and add it to project waarneming
+resource "packet_device" "instance" {
+  hostname         = "tf.ubuntu"
+  plan             = "baremetal_0"
+  facility         = "ams1"
+  operating_system = "ubuntu_16_04"
+  billing_cycle    = "hourly"
+  project_id       = "${packet_project.waarneming.id}"
+}
