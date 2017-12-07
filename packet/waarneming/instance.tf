b245f964a9f48e776316fa0e5bea68fcaffba761
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2017-12-07 08:56:42 +0100

    To baremetal_1

--- packet/waarneming/instance.tf
+++ packet/waarneming/instance.tf
@@ -11,7 +11,7 @@ resource "packet_project" "waarneming" {
 # Create a device and add it to project waarneming
 resource "packet_device" "instance" {
   hostname         = "tf.ubuntu"
-  plan             = "baremetal_0"
+  plan             = "baremetal_1"
   facility         = "ams1"
   operating_system = "ubuntu_16_04"
   billing_cycle    = "hourly"
